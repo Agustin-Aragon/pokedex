21a8c942603442e74ae8527a665b544431824beb
Agustin Aragon <agusaragon35@gmail.com>
2024-05-29 02:33:16 -0300

    filtro

--- script.js
+++ script.js
@@ -1,104 +1,100 @@
 document.addEventListener('DOMContentLoaded', function () {
-    const contenedor = document.querySelector('.contenedor')
+
+    const region = document.querySelectorAll('.region')
+    const filtro = document.querySelectorAll('.btn-filtro')
+
     mostrarPokemon(1, 151)
-    const todos = document.getElementById('todos')
-    const kanto = document.getElementById('kanto')
-    const johto = document.getElementById('johto')
-    const hoenn = document.getElementById('hoenn')
-    const sinnoh = document.getElementById('sinnoh')
-    const unova = document.getElementById('unova')
-    const kalos = document.getElementById('kalos')
-    const alola = document.getElementById('alola')
-    const galar = document.getElementById('galar')
-    const paldea = document.getElementById('paldea')
 
-    todos.addEventListener('click', function() {
-        contenedor.innerHTML = ''
-        mostrarPokemon(1, 1025)
-    })
+    region.forEach(btn => btn.addEventListener('click', Event => {
+        const region = Event.currentTarget.id
 
-    kanto.addEventListener('click', function() {
-        contenedor.innerHTML = ''
-        mostrarPokemon(1, 151)
-    })
+        switch (region) {
+            case 'todos':
+            mostrarPokemon(1, 1025)
+            break;
+            case 'kanto':
+                mostrarPokemon(1, 151)
+            break;
+            case 'johto':
+                mostrarPokemon(152, 251)
+            break;
+            case 'hoenn':
+                mostrarPokemon(252, 386)
+            break;
+            case 'sinnoh':
+                mostrarPokemon(387, 493)
+            break;
+            case 'unova':
+                mostrarPokemon(494, 649)
+            break;
+            case 'kalos':
+                mostrarPokemon(650, 721)
+            break;
+            case 'alola':
+                mostrarPokemon(722, 809)
+            break;
+            case 'galar':
+                mostrarPokemon(810, 905)
+            break;
+            case 'paldea':
+                mostrarPokemon(906, 1025)
+            break;
+        }
+    }))
 
-    johto.addEventListener('click', function() {
-        contenedor.innerHTML = ''
-        mostrarPokemon(152, 251)
-    })
+    filtro.forEach(btn => btn.addEventListener('click', Event => {
+        const filtro_tipo = Event.currentTarget.id
 
-    hoenn.addEventListener('click', function() {
-        contenedor.innerHTML = ''
-        mostrarPokemon(252, 386)
-    })
+            const tarjeta = document.querySelectorAll('.card')
+            const ocultar_tarjetas = document.querySelectorAll('.card')
+            const sin_filtro = document.querySelectorAll('.card')
 
-    sinnoh.addEventListener('click', function() {
-        contenedor.innerHTML = ''
-        mostrarPokemon(387, 493)
-    })
-    
-    unova.addEventListener('click', function() {
-        contenedor.innerHTML = ''
-        mostrarPokemon(494, 649)
-    })
-    
-    kalos.addEventListener('click', function() {
-        contenedor.innerHTML = ''
-        mostrarPokemon(650, 721)
-    })
-    
-    alola.addEventListener('click', function() {
-        contenedor.innerHTML = ''
-        mostrarPokemon(722, 809)
-    })
+            ocultar_tarjetas.forEach(ocultar => {
+                ocultar.style.display = 'none'
+            })
 
-    galar.addEventListener('click', function() {
-        contenedor.innerHTML = ''
-        mostrarPokemon(810, 905)
-    })
-
-    paldea.addEventListener('click', function() {
-        contenedor.innerHTML = ''
-        mostrarPokemon(906, 1025)
-    })
-
-    function mostrarPokemon (inicio, fin) {
-        const promesas = []
-    
-            for (let i = inicio; i <= fin; i++) {
-    
-                promesas.push(
-                    fetch(`https://pokeapi.co/api/v2/pokemon/${i}`)
-                    .then(res => res.json())
-                )
-            }
-
-            Promise.all(promesas)
-            
-            .then(data => {
-                data.forEach(data => {
-                    tarjeta(data.id, data.name, data.types, data.height, data.weight)
+            tarjeta.forEach(tarjeta => {
+                const tipos = tarjeta.querySelectorAll('.tipo')
+                tipos.forEach(tipo => {
+                    if(tipo.classList.contains(filtro_tipo)) {
+                        tarjeta.style.display = ''
+                    }
                 })
             })
             
-            .catch(error => {
-                console.log('Hubo un error:', error)
-            })
+            if(filtro_tipo.includes('todos')) {
+                sin_filtro.forEach(filtro => {
+                    filtro.style.display = ''
+                })
+            }
+        }))
+})
+
+function mostrarPokemon (inicio, fin) {
+    const contenedor = document.querySelector('.contenedor')
+    contenedor.innerHTML = ''
+    const promesas = []
+
+        for (let i = inicio; i <= fin; i++) {
+
+            promesas.push(
+                fetch(`https://pokeapi.co/api/v2/pokemon/${i}`)
+                .then(res => res.json())
+            )
         }
 
-    // for (let i = inicio; i <= fin; i++) {
-
-        //     fetch(`https://pokeapi.co/api/v2/pokemon/${i}`)
-        //     .then(res => res.json())
-        //     .then(data => {
-        //         tarjeta(data.id, data.name, data.types, data.height, data.weight)
-        //     })
-        //     .catch(error => {
-        //         console.log('Hubo un error:', error)
-        //     })
-        // }
-
-})
+        Promise.all(promesas)
+        
+        .then(data => {
+            data.forEach(data => {
+                tarjeta(data.id, data.name, data.types, data.height, data.weight)
+            })
+        })
+        
+        .catch(error => {
+            console.log('Hubo un error:', error)
+        })
+    }
 
 function tarjeta(d_id, d_name, d_type, d_height, d_weight) {
     const contenedor = document.querySelector('.contenedor')
@@ -112,7 +108,8 @@ function tarjeta(d_id, d_name, d_type, d_height, d_weight) {
                     id = d_id
                 }
 
-    if (d_type.length == 1) {
+    let tipo = d_type.map(type => `<div class="tipo ${type.type.name}">${type.type.name}</div>`)
+
         const tarjeta =
                     `<div class="card ${d_type[0].type.name}" id="${d_id}">
                         <div class="contenedor_img">
@@ -122,7 +119,7 @@ function tarjeta(d_id, d_name, d_type, d_height, d_weight) {
                         <div class="card-body">
                             <h5 class="card-title nombre">${d_name}</h5>
                             <div class="contenedor_tipos">
-                                <div class="tipo ${d_type[0].type.name}">${d_type[0].type.name}</div>
+                                ${tipo}
                             </div>
                             <div class="estadisticas">
                                 <div class="altura">
@@ -139,33 +136,5 @@ function tarjeta(d_id, d_name, d_type, d_height, d_weight) {
                     </div>
                     `
                 contenedor.insertAdjacentHTML('beforeend', tarjeta)
-    } else {
-        const tarjeta =
-                    `<div class="card ${d_type[0].type.name}" id=${d_id}>
-                        <div class="contenedor_img">
-                            <p class="id">#${id}</p>
-                            <img src=${`https://www.serebii.net/pokemongo/pokemon/${id}.png`} class="img_pokemon" alt="pokemon">
-                        </div>
-                        <div class="card-body">
-                            <h5 class="card-title nombre">${d_name}</h5>
-                            <div class="contenedor_tipos">
-                                <div class="tipo ${d_type[0].type.name}">${d_type[0].type.name}</div>
-                                <div class="tipo ${d_type[1].type.name}">${d_type[1].type.name}</div>
-                            </div>
-                            <div class="estadisticas">
-                                <div class="altura">
-                                    <p class="e_borde">altura</p>
-                                    <p id="altura">${(d_height / 10)} m</p>
-                                </div>
-                                <div class="peso">
-                                    <p class="e_borde">peso</p>
-                                    <p id="kilos">${(d_weight / 10)} kg</p>
-                                </div>
-                            </div>
-                            <a href="#" class="btn btn-primary boton">Mas detalles</a>
-                        </div>
-                    </div>
-                    `
-                contenedor.insertAdjacentHTML('beforeend', tarjeta)
-    }
+                
 }
